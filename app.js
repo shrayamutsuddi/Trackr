5685e037c2a30cabf871172f54020adb6058addc
shraya mutsuddi <mutsuddishreya@gmail.com>
2026-02-23 15:09:12 +0530

    added css

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set("views", path.join(__dirname,"views"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.use(express.static("public"))
 //mount routes before starting server
 app.use("/api", allRoutes);
 
